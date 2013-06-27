128b377c58a824ff2b1db30eb7a57d733991614e
matthesrieke <m.rieke@52north.org>
2013-06-27 17:02:25 +0200

    include Point parsing

--- track-adapter/src/main/java/org/n52/gep/inbound/cario/TrackInboundAdapter.java
+++ track-adapter/src/main/java/org/n52/gep/inbound/cario/TrackInboundAdapter.java
@@ -1,3 +1,25 @@
+/**
+ * Copyright (C) 2004
+ * by 52 North Initiative for Geospatial Open Source Software GmbH
+ *
+ * Contact: Andreas Wytzisk
+ * 52 North Initiative for Geospatial Open Source Software GmbH
+ * Martin-Luther-King-Weg 24
+ * 48155 Muenster, Germany
+ * info@52north.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.n52.gep.inbound.cario;
 
 import java.io.IOException;
@@ -21,6 +43,7 @@ import com.esri.ges.core.geoevent.FieldException;
 import com.esri.ges.core.geoevent.GeoEvent;
 import com.esri.ges.core.geoevent.GeoEventDefinition;
 import com.esri.ges.messaging.MessagingException;
+import com.esri.ges.spatial.Geometry;
 import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 
 public class TrackInboundAdapter extends AbstractInboundAdapter {
@@ -34,12 +57,16 @@ public class TrackInboundAdapter extends AbstractInboundAdapter {
 
 	private static final String FEATURES_KEY = "features";
 	private static final String PROPERTIES_KEY = "properties";
+	private static final String PHENOMENONS_KEY = "phenomenons";
+	private static final String VALUE_KEY = "value";
+	
 	private static final String[] PHENOMENON_FIELDS = new String [] {
 		TrackInboundDefinition.CONSUMPTION_KEY,
 		TrackInboundDefinition.CO2_KEY,
 		TrackInboundDefinition.SPEED_KEY,
 		TrackInboundDefinition.MAF_KEY
 	};
+
 
 	public TrackInboundAdapter(AdapterDefinition definition)
 			throws ComponentException {
@@ -104,7 +131,7 @@ public class TrackInboundAdapter extends AbstractInboundAdapter {
 		String type = (String) mapFeature.get(TrackInboundDefinition.TYPE_KEY);
 		geoEvent.setField(TrackInboundDefinition.TYPE_KEY, type);
 		
-		String geometry = createGeometry((Map<?, ?>) mapFeature.get(TrackInboundDefinition.GEOMETRY_KEY));
+		Geometry geometry = createGeometry((Map<?, ?>) mapFeature.get(TrackInboundDefinition.GEOMETRY_KEY));
 		geoEvent.setField(TrackInboundDefinition.GEOMETRY_KEY, geometry);
 
 		Map<?, ?> props = (Map<?, ?>) mapFeature.get(PROPERTIES_KEY);
@@ -116,13 +143,18 @@ public class TrackInboundAdapter extends AbstractInboundAdapter {
 		}
 		geoEvent.setField(TrackInboundDefinition.TIME_KEY, time == null ? new Date() : time);
 		
+		Map<?, ?> phens = (Map<?, ?>) props.get(PHENOMENONS_KEY);
+		
 		for (String key : PHENOMENON_FIELDS) {
-			geoEvent.setField(key, mapFeature.get(key));
+			geoEvent.setField(key, ((Map<?, ?>) phens.get(key)).get(VALUE_KEY));
 		}
 	}
 
-	private String createGeometry(Map<?, ?> object) {
-		
+	private Geometry createGeometry(Map<?, ?> object) {
+		String type = (String) object.get("type");
+		if (type.equals("Point")) {
+			return PointImpl.fromList((List<?>) object.get("coordinates"));
+		}
 		return null;
 	}
 
