568acab176ac0c30f27da3c31d27f5fb1c2bfc28
matthesrieke <m.rieke@52north.org>
2013-07-12 14:34:43 +0200

    restructure, refactor

---
+++ agol-components/js/enviroCarMap.js
@@ -0,0 +1,257 @@
+dojo.require("dijit.layout.BorderContainer");
+dojo.require("dijit.layout.ContentPane");
+dojo.require("dojo.data.ItemFileReadStore");
+dojo.require("dijit.form.FilteringSelect");
+dojo.require("esri.map");
+dojo.require("esri.layers.FeatureLayer");
+dojo.require("esri.tasks.GenerateRendererTask");
+dojo.require("esri.dijit.Legend");
+dojo.require("esri.dijit.Popup");
+dojo.require("esri.layers.agsdynamic");
+
+//the application
+var app = {};
+
+var rendererDefault = null;
+var rendererBig = null;
+var previousLod = 11;
+
+function init() {
+	esri.config.defaults.io.proxyUrl = "/proxy";
+
+	app.dataUrl = "http://ags.dev.52north.org:6080/arcgis/rest/services/enviroCar/enviroCarTracks/MapServer/0";
+
+	app.map = new esri.Map("map", {
+		center : [ 7.633, 51.9585 ],
+		zoom : previousLod,
+		basemap : "gray",
+		slider : false,
+		infoWindow : createPopup()
+	});
+
+	prepareFeatureQuery();
+	createPopupTemplate();
+	
+	//the track layer
+	urlDyn = "http://ags.dev.52north.org:6080/arcgis/rest/services/enviroCar/enviroCarTracks/MapServer";
+	trackImageLayer = new esri.layers.ArcGISDynamicMapServiceLayer(urlDyn, {
+		id : "tracks",
+		visible : false
+	});
+	trackImageLayer.setVisibleLayers([ 0 ]);
+	app.map.addLayer(trackImageLayer);
+
+	// hide the loading icon when the track layer finishes updating
+	dojo.connect(trackImageLayer, "onUpdateEnd", function() {
+		dojo.style(dojo.byId("loading"), "visibility", "hidden");
+	});
+
+	// update renderer when field name changes
+	dojo.connect(dijit.byId("fieldNames"), "onChange", changePhenomenon);
+	dojo.connect(app.map, "onExtentChange", selectRendererOnScale);
+	dojo.connect(app.map, 'onClick', queryFeatureInfo);
+
+	// get field info
+	retrieveFields();
+}
+
+function createPopup() {
+	return new esri.dijit.Popup({
+		markerSymbol : new esri.symbol.SimpleMarkerSymbol("circle", 32, null,
+				new dojo.Color([ 0, 0, 0, 0.25 ])),
+		marginLeft : "20",
+		marginTop : "20"
+	}, dojo.create("div"));
+}
+
+function prepareFeatureQuery() {
+	app.queryTask = new esri.tasks.QueryTask(
+			"http://ags.dev.52north.org:6080/arcgis/rest/services/enviroCar/enviroCarTracks/MapServer/0");
+	app.query = new esri.tasks.Query();
+	app.query.returnGeometry = true;
+	app.query.outFields = [ "starttime", "sensor", "speed", "co2",
+			"consumption", "maf" ];
+}
+
+function retrieveFields() {
+	var trackFields = esri.request({
+		url : app.dataUrl,
+		content : {
+			f : "json"
+		},
+		callbackParamName : "callback"
+	});
+	trackFields.then(function(resp) {
+		var fieldNames, fieldStore;
+
+		fieldNames = {
+			identifier : "value",
+			label : "name",
+			items : []
+		};
+		//fields 2 to 6 define the attributes
+		dojo.forEach(resp.fields.slice(2, 6), function(f) {
+			fieldNames.items.push({
+				"name" : f.name,
+				"value" : f.name
+			});
+		});
+		fieldStore = new dojo.data.ItemFileReadStore({
+			data : fieldNames
+		});
+		dijit.byId("fieldNames").set("store", fieldStore);
+		dijit.byId("fieldNames").set("value", "speed"); // set a value
+	}, function(err) {
+		console.log("failed to get field names: ", err);
+	});
+}
+
+function createPopupTemplate() {
+	app.popupTemplate = new esri.dijit.PopupTemplate({
+		title : "{sensor}",
+		fieldInfos : [ {
+			fieldName : "starttime",
+			visible : true,
+			label : "Time",
+			format : {
+				dateFormat : 'shortDateShortTime24'
+			}
+		}, {
+			fieldName : "speed",
+			visible : true,
+			label : "Speed"
+		}, {
+			fieldName : "co2",
+			visible : true,
+			label : "CO2 Emission"
+		}, {
+			fieldName : "consumption",
+			visible : true,
+			label : "Fuel Consumption"
+		}, {
+			fieldName : "maf",
+			visible : true,
+			label : "Calculated MAF"
+		} ]
+	});
+}
+
+function queryFeatureInfo(e) {
+	// build an extent around the click point
+	var pad = app.map.extent.getWidth() / app.map.width * 3;
+	var queryGeom = new esri.geometry.Extent(e.mapPoint.x - pad, e.mapPoint.y
+			- pad, e.mapPoint.x + pad, e.mapPoint.y + pad,
+			app.map.spatialReference);
+	app.query.geometry = queryGeom;
+
+	var def = app.queryTask.execute(app.query);
+	def.addCallback(function(result) {
+		return dojo.map(result.features, function(f) {
+			f.setInfoTemplate(app.popupTemplate);
+			return f;
+		});
+	});
+
+	app.map.infoWindow.setFeatures([ def ]);
+	app.map.infoWindow.show(e.screenPoint, app.map
+			.getInfoWindowAnchor(e.screenPoint));
+}
+
+function selectRendererOnScale(extent, delta, outLevelChange, outLod) {
+	if (outLod.level > 14 && previousLod <= 14) {
+		applyRenderer(true);
+	} else if (outLod.level <= 14 && previousLod > 14) {
+		applyRenderer(false);
+	}
+
+	previousLod = outLod.level;
+}
+
+function changePhenomenon() {
+	dojo.style(dojo.byId("loading"), "visibility", "visible");
+	createRenderer(dijit.byId("fieldNames").get("value") || "speed");
+}
+
+function createRenderer(phenomenonField) {
+	var request, requestBig;
+	if (phenomenonField == "maf") {
+		request = esri.request({
+			url : "data/rendererMAF.json",
+			handleAs : "json"
+		});
+		requestBig = esri.request({
+			url : "data/rendererBigMAF.json",
+			handleAs : "json"
+		});
+	} else if (phenomenonField == "co2") {
+		request = esri.request({
+			url : "data/rendererCo2.json",
+			handleAs : "json"
+		});
+		requestBig = esri.request({
+			url : "data/rendererBigCo2.json",
+			handleAs : "json"
+		});
+	} else if (phenomenonField == "consumption") {
+		request = esri.request({
+			url : "data/rendererConsumption.json",
+			handleAs : "json"
+		});
+		requestBig = esri.request({
+			url : "data/rendererBigConsumption.json",
+			handleAs : "json"
+		});
+	} else {
+		request = esri.request({
+			url : "data/rendererSpeed.json",
+			handleAs : "json"
+		});
+		requestBig = esri.request({
+			url : "data/rendererBigSpeed.json",
+			handleAs : "json"
+		});
+	}
+	request.then(function(response) {
+		rendererDefault = new esri.renderer.ClassBreaksRenderer(response);
+		applyRenderer(previousLod > 14);
+	});
+	requestBig.then(function(response) {
+		rendererBig = new esri.renderer.ClassBreaksRenderer(response);
+		applyRenderer(previousLod > 14);
+	});
+}
+
+function applyRenderer(useBig) {
+	var renderer;
+	if (useBig) {
+		renderer = rendererBig;
+	} else {
+		renderer = rendererDefault;
+	}
+	var optionsArray = [];
+	var drawingOptions = new esri.layers.LayerDrawingOptions();
+	drawingOptions.renderer = renderer;
+	optionsArray[0] = drawingOptions;
+	app.map.getLayer("tracks").setLayerDrawingOptions(optionsArray);
+	app.map.getLayer("tracks").show();
+	if (!app.hasOwnProperty("legend")) {
+		createLegend();
+	}
+}
+
+function createLegend() {
+	app.legend = new esri.dijit.Legend({
+		map : app.map,
+		layerInfos : [ {
+			layer : app.map.getLayer("tracks"),
+			title : "enviroCar Tracks"
+		} ]
+	}, dojo.byId("legendDiv"));
+	app.legend.startup();
+}
+
+function errorHandler(err) {
+	console.log("error: ", dojo.toJson(err));
+}
+
+dojo.ready(init);
