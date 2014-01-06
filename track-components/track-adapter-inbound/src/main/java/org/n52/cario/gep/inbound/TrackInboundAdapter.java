5fd01186910bfe9afe2445e71934794472277103
matthesrieke <m.rieke@52north.org>
2014-01-06 12:17:18 +0100

    add logging statements for debugging purposes

--- track-components/track-adapter-inbound/src/main/java/org/n52/cario/gep/inbound/TrackInboundAdapter.java
+++ track-components/track-adapter-inbound/src/main/java/org/n52/cario/gep/inbound/TrackInboundAdapter.java
@@ -96,8 +96,10 @@ public class TrackInboundAdapter extends AbstractInboundAdapter {
 	@Override
 	protected synchronized GeoEvent createGeoEvent(InputStream input) {
 		try {
+			logger.info("Queue size before: {}", this.eventQueue.size());
 			Map<?, ?> json = JsonUtil.createJson(input);
 			appendFeaturesToQueue(json);
+			logger.info("Queue size after: {}", this.eventQueue.size());
 			return this.eventQueue.poll();
 		} catch (RuntimeException e) {
 			logger.warn(e.getMessage(), e);
@@ -124,6 +126,7 @@ public class TrackInboundAdapter extends AbstractInboundAdapter {
 			return;
 
 		List<?> features = (List<?>) json.get(FEATURES_KEY);
+		logger.info("# of features in track's JSON: {}", features.size());
 		Map<?, ?> props = (Map<?, ?>) json.get(PROPERTIES_KEY);
 		
 		String sensor = parseSensor(props);
